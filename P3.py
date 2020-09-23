d3b78ad094dfbe8b1c0ce0e8c1904da1669129a9
madaravali <70214978+madaravali@users.noreply.github.com>
2020-09-23 11:02:29 -0700

    Create P3.py

---
+++ P3.py
@@ -0,0 +1,9 @@
+a=int(input())
+d=[int(i) for i in input().split()]
+for i in range(len(d)):
+    sum=d[i]
+    for j in range(i+1,len(d)):
+        sum=sum+d[j]
+        if(sum==a):
+            print(i)
+            print(j)
